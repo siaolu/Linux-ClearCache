ebc3625e3d820df3eb5381671950722ae26ad988
siaOlu <oluwasegun@darqbx.com>
2019-07-30 01:35:36 -0500

    revised microsrvc dirtree

---
+++ memdx/clrcachev2.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+#title          : clrcachev2.sh
+#description    : clear/flush drop_cache
+#FusedBy        : sia-emff
+#date           : 201913JUL
+#version        : v0.2    
+
+#==============================================================================
+
+### StringSubSetups
+runlog=/var/log/log.clrcache
+
+dropcaches="/proc/sys/vm/drop_caches"
+# Adjust per your requirements
+# 3. Clear PageCache, dentries and inodes
+# 2. Clear dentries and inodes.
+# 1. Clear PageCache only.
+FlushLvl="3"
+xsn=" sync; "
+xec=" echo "
+tdate=$(date)
+vms=" vmstat"
+vmo=" -a -m -w -n "
+
+### - For use when appending features & debugging 
+#$xec "status: string setups done." 
+
+fx_runstat () {
+    $xec "status: ${1} done."
+}
+
+log_header () {
+    $xec "=== $tdate ===" >> $runlog
+}
+
+log_vmstats () {            #Dump vmstat d ata to log
+    $vms $vmo >> $runlog
+    free -g >> $runlog
+    #fx_runstat "log_vmstats"
+}
+
+fxclearCache () {            #Clear cache
+    $scn$xec$FlushLvl > $dropcaches
+    #fx_runstat "fxclearCache"
+}
+
+fxbounceSwap () {            #Clear Swap - Caution see Readme.md
+  swapoff -a && swapon -a
+  #fx_runstat "fxbounceSwap"
+}
+
+log_end () {
+  echo "---Cache cleared" >> $runlog
+}
+
+exec_main () {
+    log_header
+    fxclearCache
+    fxbounceSwap
+    log_vmstats
+    log_end
+    #fx_runstat "exec_main"
+}
+
+exec_main
